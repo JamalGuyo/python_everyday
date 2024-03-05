b2c8cacdb5fc6b11a27a426c036b2030f3f31fd2
JamalGuyo <jamalguyo08@gmail.com>
2024-03-05 09:09:26 +0300

    write python program to be tested

---
+++ day_9/calculator.py
@@ -0,0 +1,19 @@
+"""
+unit testing in python
+This module contains functions to be tested
+"""
+
+
+def main():
+    """main function"""
+    x = int(input("What's x?"))
+    print(f"x squared is {square(x)}")
+
+
+def square(n):
+    """square function"""
+    return n * n
+
+
+if __name__ == "__main__":
+    main()
