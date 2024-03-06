b5d8443445da7e1265e3a2db61eb6120aa385ba2
JamalGuyo <jamalguyo08@gmail.com>
2024-03-06 09:00:34 +0300

    short hand read lines of a file

--- day_10/names.py
+++ day_10/names.py
@@ -9,7 +9,5 @@ File I/O
 
 
 with open("name.txt", "r") as file:
-    lines = file.readlines()
-
-for line in lines:
-    print("Hello,", line.rstrip())
+    for line in file:
+        print("Hello,", line.rstrip())
