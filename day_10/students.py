6ebaf7a66c0d51f3634b11f10edc6824cfc101a3
JamalGuyo <jamalguyo08@gmail.com>
2024-03-06 09:12:32 +0300

    sorting csv output

--- day_10/students.py
+++ day_10/students.py
@@ -2,7 +2,14 @@
 FIle I/O from csv files
 """
 
+students = []
+
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
+        student = {"name": name, "house": house}
+        students.append(student)
         print(f"{name} is in {house}")
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is in {student['house']}")
