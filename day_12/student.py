4162947966d0bf4bc1990193080f323dba2ac490
JamalGuyo <jamalguyo08@gmail.com>
2024-03-08 08:25:33 +0300

    using tuples: immutable

---
+++ day_12/student.py
@@ -0,0 +1,18 @@
+"""
+classes in python
+"""
+
+
+def main():
+    student = get_student()
+    print(f"{student[0]} is in {student[1]}")
+
+
+def get_student():
+    name = input("What's your name?")
+    house = input("What's your house?")
+    return name, house
+
+
+if __name__ == "__main__":
+    main()
